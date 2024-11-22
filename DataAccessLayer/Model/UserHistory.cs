ed22ca09c049bf728454ea6dc342ae195212c842
jeevanandh-kanini <jeevanandh.kanini@gmail.com>
2024-11-22 11:45:08 +0530

    UserHistory  model

---
+++ DataAccessLayer/Model/UserHistory.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Model
+{
+    public class UserHistory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual Users Users { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+
+        [ForeignKey("BookId")]
+        public virtual Books Books { get; set; }
+
+        [Required]
+        public DateTime RequestDate { get; set; }
+
+        [Required]
+        public DateTime Returndate { get; set; }
+
+        public string Review { get; set; }
+    }
+}
